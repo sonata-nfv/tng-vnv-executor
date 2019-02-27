8c334a946f939f8e5655e753a34ada7948350954
Laura Alvarez <lalvarez@optaresolutions.com>
2019-02-27 14:07:01 +0100

    Scheduled task, to delete old testExecutions (currently, a week old tests)

--- src/main/groovy/app/database/TestExecutionRepository.groovy
+++ src/main/groovy/app/database/TestExecutionRepository.groovy
@@ -1,6 +1,14 @@
 package app.database
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.Temporal
+import org.springframework.data.repository.query.Param
+
+import javax.persistence.TemporalType
 
 interface TestExecutionRepository extends JpaRepository<TestExecution, String> {
+
+    @Query("Select t from TestExecution t where t.created <= :limitDate")
+    Collection<TestExecution> findOldTestExecutions(@Param("limitDate") @Temporal(value = TemporalType.TIMESTAMP) Date limitDate);
 }
