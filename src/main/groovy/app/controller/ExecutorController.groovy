e85289ba181e8555876b8bd0927f9238e4a6b79d
Santiago Rodriguez Garcia <srg@mail.uni-paderborn.de>
2019-05-14 10:03:13 +0200

    test request received trace

--- src/main/groovy/app/controller/ExecutorController.groovy
+++ src/main/groovy/app/controller/ExecutorController.groovy
@@ -95,7 +95,7 @@ class ExecutorController {
 
         def message = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(test)
         logger.info("test request received from curator: ", message)
-        logger.info("message.toString(): ${message.toString()}")
+        logger.info("test.toString(): ${test.toString()}")
 
         //get TD
         TestDescriptor testDescriptor = test.getTest()
